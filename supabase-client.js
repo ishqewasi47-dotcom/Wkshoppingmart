ecaf90f858972de86ee193923af3ddfcf279a9b4
ishqewasi47-dotcom <ishqewasi47@gmail.com>
2026-09-22 04:33:38 +0500

    Update supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -6,8 +6,8 @@ const SUPABASE_URL = "https://uljtejooevxuonfziudy.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_3kRRIwf2rQ3-YVR95dx0hA_vOOj23Dm";
 
 // WhatsApp number jahan order details forward hongi (country code ke sath, + ke bina)
-// e.g. Pakistan number 03001234567 -> "923001234567"
-const STORE_WHATSAPP_NUMBER = "923001234567";
+// e.g. Pakistan number +923453238159 -> "923001234567"
+const STORE_WHATSAPP_NUMBER = "+923453238159";
 
 // Storage bucket name jo product images ke liye Supabase mein banayenge
 const PRODUCT_IMAGE_BUCKET = "product-images";
